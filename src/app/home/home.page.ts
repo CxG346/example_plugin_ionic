b6bb7b582f6fa740ed21bf820b3ec4ad3535df7c
CxG346 <dgm3036@gmail.com>
2024-08-20 15:49:30 -0500

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { InAppBrowser, ToolBarType } from '@capgo/inappbrowser';
+import { OpenWebViewOptions, NewOpenWebOptions } from '@capgo/inappbrowser';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,88 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
   constructor() {}
 
+  ngOnit() {}
+
+  async testOpen() {
+    const options = {
+      url: 'https://es.wikipedia.org',
+    };
+
+    try {
+      await InAppBrowser.open(options);
+    } catch (error) {
+      console.error('Failed to open browser:', error);
+    }
+  }
+
+  async testOpenWebView() {
+    const options: OpenWebViewOptions = {
+      url: 'https://es.wikipedia.org',
+      headers: { 'Custom-Header': 'value' },
+      shareDisclaimer: {
+        title: 'Disclaimer',
+        message: 'Share this page',
+        confirmBtn: 'Confirm',
+        cancelBtn: 'Cancel',
+      },
+      toolbarType: undefined, // or assign a valid value of type ToolBarType
+      shareSubject: 'Check this out',
+      title: 'New Window',
+      activeNativeNavigationForWebview: true,
+      disableGoBackOnNativeApplication: false,
+      isPresentAfterPageLoad: true,
+      isInspectable: false,
+      isAnimated: true,
+      showReloadButton: true,
+      closeModal: true,
+      closeModalTitle: 'Close',
+      closeModalDescription: 'Are you sure you want to close this window?',
+      closeModalOk: 'Close',
+      closeModalCancel: 'Cancel',
+      visibleTitle: true,
+      toolbarColor: 'red',
+      showArrow: false,
+      ignoreUntrustedSSLError: false,
+    };
+
+    try {
+      await InAppBrowser.openWebView(options);
+    } catch (error) {
+      console.error('Failed to open browser:', error);
+    }
+  }
+
+  async testNewOpen() {
+    const options: NewOpenWebOptions = {
+      url: 'https://wikipedia.com',
+      title: 'Mi Navegador Test',
+      showShareButton: true,
+      showDownloadButton: true,
+      showNavigationButtons: true,
+      toolbarType: ToolBarType.NAVIGATION,
+    };
+
+    try {
+      await InAppBrowser.openWeb(options);
+
+      await InAppBrowser.addListener('shareButtonClicked', () => {
+        console.log('El botón de compartir fue presionado');
+        alert('El botón de compartir fue presionado Porfin');
+      });
+
+      await InAppBrowser.addListener('downloadButtonClicked', () => {
+        alert('El botón de download fue presionado Porfin');
+      });
+    } catch (error) {
+      console.error('Failed to open browser:', error);
+      alert('Failed to open browser: ' + error);
+    }
+  }
+
+  async alertFunction() {
+    console.log('console log called')
+    alert('Alert function called');
+  }
 }
